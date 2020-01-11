37f52e5a33fe8cd676a20e4ef9b986bb3caff213
Marcin <mwandalowski@wp.pl>
2020-01-11 22:50:07 +0100

    About us section completed

--- oddaj-rzeczy/src/components/Home/AboutUs.js
+++ oddaj-rzeczy/src/components/Home/AboutUs.js
@@ -4,9 +4,23 @@ class AboutUs extends Component {
   state = {};
   render() {
     return (
-      <>
-        <h1>About us</h1>
-      </>
+      <section className="aboutUs">
+        <div className="row">
+          <div className="aboutUsLeft col-8">
+            <h3>O nas</h3>
+            <div className="decorationImage3"></div>
+            <p>
+              Integer id felis ut lacus dignissim suscipit in ut quam. Sed
+              tempus et eros at imperdiet. Nulla facilisi. Integer suscipit
+              fringilla sapien pretium aliquam. Morbi eleifend lobortis nisi.
+            </p>
+            <div className="aboutUsSignature"></div>
+          </div>
+          <div className="aboutUsRight col-8">
+            <div className="aboutUsPhoto"></div>
+          </div>
+        </div>
+      </section>
     );
   }
 }

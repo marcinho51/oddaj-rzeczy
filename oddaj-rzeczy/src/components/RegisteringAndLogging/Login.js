04395fbf291a679a6ef906bba3337a152206c2da
Marcin <mwandalowski@wp.pl>
2020-01-14 21:39:41 +0100

    Login validation done

--- oddaj-rzeczy/src/components/RegisteringAndLogging/Login.js
+++ oddaj-rzeczy/src/components/RegisteringAndLogging/Login.js
@@ -3,7 +3,55 @@ import Navigation from "../Home/Navigation";
 import Logging from "../Home/Logging";
 import { Link } from "react-router-dom";
 class Login extends Component {
-  state = {};
+  state = {
+    email: "",
+    password: "",
+    emailValidation: true,
+    passwordValidation: true,
+    formValidation: false
+  };
+
+  checkEmail = event => {
+    this.setState({
+      email: event.target.value
+    });
+  };
+
+  checkPassword = event => {
+    this.setState({
+      password: event.target.value
+    });
+  };
+
+  checkForm = event => {
+    if (this.state.email.length < 7) {
+      this.setState({ emailValidation: false });
+    }
+
+    if (this.state.email.indexOf("@") === -1) {
+      this.setState({ emailValidation: false });
+    }
+
+    if (this.state.email.indexOf(" ") !== -1) {
+      this.setState({ emailValidation: false });
+    }
+
+    if (this.state.password.length < 6) {
+      this.setState({ passwordValidation: false });
+    }
+
+    if (
+      this.state.email.length >= 7 &&
+      this.state.email.indexOf("@") !== -1 &&
+      this.state.email.indexOf(" ") === -1 &&
+      this.state.password.length >= 6
+    ) {
+      this.setState({ formValidation: true });
+    }
+
+    event.preventDefault();
+  };
+
   render() {
     return (
       <section className="login">
@@ -21,11 +69,37 @@ class Login extends Component {
             <form>
               <label>
                 Email
-                <input type="email" />
+                <input
+                  type="email"
+                  value={this.state.email}
+                  onChange={this.checkEmail}
+                  style={
+                    this.state.emailValidation
+                      ? { borderBottom: "1px solid $color-black" }
+                      : { borderBottom: "1px solid red" }
+                  }
+                />
+                {this.state.emailValidation ? null : (
+                  <p style={{ color: "red" }}>
+                    Podany email jest nieprawidłowy!
+                  </p>
+                )}
               </label>
               <label>
                 Hasło
-                <input type="password" />
+                <input
+                  type="password"
+                  value={this.state.password}
+                  onChange={this.checkPassword}
+                  style={
+                    this.state.passwordValidation
+                      ? { borderBottom: "1px solid $color-black" }
+                      : { borderBottom: "1px solid red" }
+                  }
+                />
+                {this.state.passwordValidation ? null : (
+                  <p style={{ color: "red" }}>Podane hasło jest za krótkie!</p>
+                )}
               </label>
             </form>
           </div>
@@ -33,8 +107,10 @@ class Login extends Component {
             <button>
               <Link to="/rejestracja">Załóż konto</Link>
             </button>
-            <button>
-              <Link to="/logowanie">Zaloguj się</Link>
+            <button onClick={this.checkForm}>
+              {/* <Link to="/logowanie"> */}
+              Zaloguj się
+              {/* </Link> */}
             </button>
           </div>
         </div>

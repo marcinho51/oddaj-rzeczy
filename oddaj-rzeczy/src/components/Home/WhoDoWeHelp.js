0730c87a6494862f5b6eae66927ee4de596a712f
Marcin <mwandalowski@wp.pl>
2020-01-21 23:11:21 +0100

    Beginning of pagination done

--- oddaj-rzeczy/src/components/Home/WhoDoWeHelp.js
+++ oddaj-rzeczy/src/components/Home/WhoDoWeHelp.js
@@ -3,7 +3,9 @@ import React, { Component } from "react";
 class Home_Who_Do_We_Help extends Component {
   state = {
     fundations: [],
-    currentOrganisation: 1
+    currentOrganisation: 1,
+    currentPage: 0,
+    itemPerPage: 3
   };
   componentDidMount() {
     fetch("http://localhost:3001/fundations")
@@ -35,29 +37,28 @@ class Home_Who_Do_We_Help extends Component {
   }
 
   get items() {
-    const fun = this.state.fundations.find(
+    return this.state.fundations.find(
       fundation => fundation.id == this.state.currentOrganisation
-    );
-    console.log(fun);
-    return (
-      fun &&
-      fun.items.map(fundation => {
-        return (
-          <div key={fundation.header} className="fundationMain row">
-            <div className="fundationHeader col-7">{fundation.header}</div>
-            <div className="col-6"></div>
-            <div className="fundationDesc col-3">{fundation.desc}</div>
-            <div className="fundationSubheader col-7">
-              {fundation.subheader}
-            </div>
-          </div>
-        );
-      })
     );
   }
 
+  get fundations() {
+    console.log(this.items[desc]);
+    const { currentPage, itemPerPage } = this.state;
+    return this.items?.items?.length
+      ? this.items.items.slice(
+          currentPage * itemPerPage,
+          currentPage * itemPerPage + itemPerPage
+        )
+      : [];
+  }
+
+  get pages() {
+    return Math.ceil(this.items?.items?.length / this.state.itemPerPage);
+  }
+
   render() {
-    console.log(this.state);
+    console.log(this.pages);
     return (
       <section className="whoDoWeHelp">
         <div className="whoDoWeHelpHeadline">
@@ -71,9 +72,26 @@ class Home_Who_Do_We_Help extends Component {
           </div>
           <div className="col-4"></div>
 
+          <div className="col-4"></div>
+          {/* <div className="col-8">{this.items.desc}</div> */}
+          <div className="col-4"></div>
+
           <div className="col-3"></div>
           <div className="whoDoWeHelpList col-10">
-            <div>{this.items}</div>
+            <div>
+              {this.fundations.map(fundation => (
+                <div key={fundation.header} className="fundationMain row">
+                  <div className="fundationHeader col-7">
+                    {fundation.header}
+                  </div>
+                  <div className="col-6"></div>
+                  <div className="fundationDesc col-3">{fundation.desc}</div>
+                  <div className="fundationSubheader col-7">
+                    {fundation.subheader}
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
           <div className="col-3"></div>
         </div>

8dd0ea3a949011c231458ac865de4365db54f7f2
Marcin <mwandalowski@wp.pl>
2020-01-23 23:48:50 +0100

    Updates

---
+++ precache-manifest.4b352607a167b2a2a25315b0bc075c95.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7603eae4b15c63659917606546db99f3",
+    "url": "/oddaj-rzeczy/index.html"
+  },
+  {
+    "revision": "df04c233cf9308120c3c",
+    "url": "/oddaj-rzeczy/static/css/main.3afb03cc.chunk.css"
+  },
+  {
+    "revision": "437892ab9492d6531713",
+    "url": "/oddaj-rzeczy/static/js/2.5cd68d4e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/oddaj-rzeczy/static/js/2.5cd68d4e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "df04c233cf9308120c3c",
+    "url": "/oddaj-rzeczy/static/js/main.f77d0ae4.chunk.js"
+  },
+  {
+    "revision": "83ad22efbbe920cc793d",
+    "url": "/oddaj-rzeczy/static/js/runtime-main.11bfd88b.js"
+  },
+  {
+    "revision": "f4db7ddd6531361fb3777e2b8fe9e8d9",
+    "url": "/oddaj-rzeczy/static/media/Background-Contact-Form.f4db7ddd.jpg"
+  },
+  {
+    "revision": "2290784ed27eb2dc06f78414add6bec8",
+    "url": "/oddaj-rzeczy/static/media/Decoration.2290784e.svg"
+  },
+  {
+    "revision": "df65b6c0ffa42176de9927a6a55fb508",
+    "url": "/oddaj-rzeczy/static/media/Facebook.df65b6c0.svg"
+  },
+  {
+    "revision": "e99903ed42896ab89edf6c2e1d2add9e",
+    "url": "/oddaj-rzeczy/static/media/Home-Hero-Image.e99903ed.jpg"
+  },
+  {
+    "revision": "91147906e8dba33519e1c0ff12d308d4",
+    "url": "/oddaj-rzeczy/static/media/Icon-1.91147906.svg"
+  },
+  {
+    "revision": "db91e33e563df406664bb2a87d9aaa04",
+    "url": "/oddaj-rzeczy/static/media/Icon-2.db91e33e.svg"
+  },
+  {
+    "revision": "2cfe9dfeec6b7c1ffba1289046649228",
+    "url": "/oddaj-rzeczy/static/media/Icon-3.2cfe9dfe.svg"
+  },
+  {
+    "revision": "436df0aa3f9a7dcda9f88d45bf1fe1a7",
+    "url": "/oddaj-rzeczy/static/media/Icon-4.436df0aa.svg"
+  },
+  {
+    "revision": "703479fdb58a909c7d696f41dc955649",
+    "url": "/oddaj-rzeczy/static/media/Instagram.703479fd.svg"
+  },
+  {
+    "revision": "6fa3665144c1acf34c84e4d6a9e6b6ed",
+    "url": "/oddaj-rzeczy/static/media/People.6fa36651.jpg"
+  },
+  {
+    "revision": "e54cfcfc6ba3bedf321fc036d8ba90e0",
+    "url": "/oddaj-rzeczy/static/media/Signature.e54cfcfc.svg"
+  }
+]);
